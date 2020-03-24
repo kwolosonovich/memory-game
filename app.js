4512e605c8ae8f6df55cc7bf448590d30f46c6f5
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-03-24 10:58:58 -0700

    added numbers to pictures - html

--- app.js
+++ app.js
@@ -2,21 +2,35 @@ var score = document.querySelector("h4.score");
 
 var image = document.querySelector("img.img-thumbnail")
 
+function setImage() {
+    for (image of images) {
+        image.source = `images/${randomImg}.png` 
+    }
+}
+
 function randomImg() {
   let set = [1, 2, 3, 4, 5, 6];
   let setMatch = [];
   let min = set[0];
   let max = set.length - 1;
 
-  for (let i = 0; i < set.length; i++) {
-    let randomNum = Math.floor(Math.random() * max);
-    setMatch.push(set[randomNum]);
-    set.splice(randomNum, 1);
-    // console.log(randomNum)
+  if (set.length > 0) {
+    for (let i = 0; i < set.length; i++) {
+      let randomNum = Math.floor(Math.random() * max);
+      setMatch.push(set[randomNum]);
+      set.splice(randomNum, 1);
+      return randomNumber;
+    }    
+  } else if (set.length === 0) {
+    for (let i = 0; i < setMatch.length; i++) {
+      let randomNum = Math.floor(Math.random() * max);
+      set.splice(randomNum, 1);
+      return randomNumber;
+    }    
   }
+
+ 
 
   //   console.log(set)
   //   console.log(setMatch)
 }
-
-randomImg();
