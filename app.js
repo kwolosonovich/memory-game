ab6b306dcc7c2cd874a9f2ece9299628f1ac21c2
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-03-25 08:42:20 -0700

    updated matching functions

--- app.js
+++ app.js
@@ -1,69 +1,95 @@
 var score = document.getElementsByClassName("score")
-var image = document.querySelectorAll("img.img-thumbnail")
+var images = document.querySelectorAll("img.img-thumbnail")
 var startGame = document.querySelector("button")
 var alertText = document.querySelector("h4.alertText")
-var imageSrcList = document.querySelectorAll("img.src")
+var imageSrcList = document.querySelectorAll("img")
 var runningScore = 0;
 
 // **Start Game
+startGame.addEventListener("click", game)
 
-startGame.addEventListener("click", function() {
-//   e.preventDefault();
-  setImage();
-});
+function game(e){
+ e.preventDefault();
+   setImage();
+}
+
 
 function setImage() {
-    for (src of imageSrcList) {
-      image.src = `images/${randomImg}.png`;
+  // console.log(imageSrcList)
+    for (let image of imageSrcList) {
+        // console.log(image);
+        // console.log(randomImg());
+
+      image.src = `images/${randomImg()}.jpg`;
     }
 }
 
 function randomImg() {
+  // console.log("randomImg");
   let set = [1, 2, 3, 4, 5, 6];
   let setMatch = [];
-  let min = set[0];
-  let max = set.length - 1;
+  let setMax = set.length;
+  let setMatchMax = setMatch.length
+
+  let result = 0
 
   if (set.length > 0) {
     for (let i = 0; i < set.length; i++) {
-      let randomNum = Math.floor(Math.random() * max);
-      setMatch.push(set[randomNum]);
-      set.splice(randomNum, 1);
-      return randomNumber;
+      let randomNumber = Math.floor(Math.random() * setMax);
+      console.log(randomNumber)
+      result = set[randomNumber]
+      setMatch.push(set[randomNumber]);
+      set.splice(result, 1);
+      return result;
     }
-  } else if (set.length === 0) {
+  } else if (set.length = 0) {
     for (let i = 0; i < setMatch.length; i++) {
-      let randomNum = Math.floor(Math.random() * max);
-      set.splice(randomNum, 1);
-      return randomNumber;
+      let randomNumber = Math.floor(Math.random() * setMatchMax);
+      setMatch.splice(setMatch[randomNumber], 1);
+      return setMatch[randomNumber];
     }
   }
 }
 
 
-// **Event listener - find matches
+// **Event listener - find matches**
 
-image.addEventListener("click", function() {
-    // e.preventDefault();
-    image.style.visiibility = visible;
-  if (pairs.firstCard === undefined) {
-    pairs.firstCard = image.src;
-  } else if (pairs.firstCard !== undefined && pairs.firstCard === undefined) {
-    image.style.visiibility = visible;
-    pairs.secondCard = image.src;
+for (let image of images) {
+  image.addEventListener("click", imageClick);
+  
+}
+
+
+function imageClick(e){
+  e.preventDefault()
+  //  image.style.visibility = visible;
+  e.target.style.opacity = "100%"
+
+  var pairs = {
+    firstCard: "undefined",
+    secondCard: "undefined"
+  };
+
+  // let compairSrc = e.target.img
+
+  if (pairs.firstCard === "undefined") {
+    pairs.firstCard = e.target.img;
+    //console.log(pairs.firstCard)
+
+
+
+  } else if (pairs.firstCard !== "undefined" && pairs.firstCard === "undefined") {
+    pairs.secondCard = document.querySelector("img");
+    console.log(pairs.secondCard)
     if (pairs.firstImage === pairs.secondImage) {
       match()
     } else if (pairs.firstImage !== pairs.secondImage) {
       notAMatch()
     }
   }
-});
+}
+// **Helper functions - find matches**
 
-// **Helper functions - find matches
-var pairs = {
-  firstCard: undefined,
-  secondCard: undefined
-};
 
 function match() {
     alert("Match!");
@@ -76,11 +102,11 @@ function notAMatch() {
     alert("Not a match. Try again!");
     pairs.firstCard = undefined;
     pairs.secondCard = undefined;
-    image.style.visiibility = "hidden";
+    image.style.visibility = "hidden";
 }
 
 
-// **Scoring
+// **Scoring**
 
 function scoreCalc() {
   runningScore += 2;
