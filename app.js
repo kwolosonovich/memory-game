012befcb0c66a3f2fa0dcb6b46ba0ba2f9472289
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-03-26 16:39:09 -0700

    updated all files

--- app.js
+++ app.js
@@ -1,103 +1,31 @@
-var score = document.getElementsByClassName("score")
-var images = document.querySelectorAll("img.img-thumbnail")
-var startGame = document.querySelector("button")
-var alertText = document.querySelector("h4.alertText")
-var imageSrcList = document.querySelectorAll("img")
+var score = document.querySelector("#score");
+var images = document.querySelectorAll("img.img-thumbnail");
+var startGame = document.querySelector("button");
+var alertText = document.querySelector("h4.alertText");
+var imageSrcList = document.querySelectorAll("img");
 var runningScore = 0;
 var imageDivs = document.querySelectorAll(".card");
-console.log(imageDivs)
 let lowScore = localStorage.getItem("low-score");
+let game = document.querySelector("#game");
+var startGame = document.querySelector("button");
 
 
+let numCards = imageDivs.length;
 
-// **Start Game
-startGame.addEventListener("click", gameStart)
+let cardsFlipped = 0;
+let currentScore = 0;
 
-// function game(e){
-//  e.preventDefault();
-//   //  setImage();
-// }
-
-
-//function setImage() {
-
-    // shuffle(setOfPics)
-    // console.log(setOfPics)
-    // for (let image of imageSrcList) {
-    // // for (let i = 0; i < pairs.length; i++) {
-    //     image.src = `images/${randomImage}.jpg`;
-    //   }
-    //   // imageSrcList.src = imagePath;
-    // for (let i = 0; i < imageDiv.length; i++) {
-    //   let path = `images/${randomImage}.jpg`;
-    //   imageDiv[i].children[0].src = path
-    // }
+let pairs = {
+  firstCard: null,
+  secondCard: null
+};
 
 
-//     return pairs[i]
-
-
-    // for (let i = 0; i < setOfPics.length; i++) {
-    //   imageSrcList.src = `imagescard"/${setOfPics[i]}.jpg`;
-    // }
-
-        // for (let image of imageSrcList) {
-        //   for (let i = 0; i < setOfPics.length; i++) {
-        //     image.src = `images/${setOfPics[i]}.jpg`;
-
-        //   }
-        // }
-          
-          // image.src = `images/${randomImg()}.jpg`;
-
-//}
-
-
-// function randomImage() {
-//   for (let i = 0; i < pairs.length; i++) {
-//     return pairs[i]
-//   }
-// }
-
-
-// function shuffle(setOfPics) {
-//   var currentIndex = setOfPics.length, temporaryValue, randomIndex;
-//   while (0 !== currentIndex) {
-//     randomIndex = Math.floor(Math.random() * currentIndex);
-//     currentIndex -= 1;
-//     temporaryValue = setOfPics[currentIndex];
-//     setOfPics[currentIndex] = setOfPics[randomIndex];
-//     setOfPics[randomIndex] = temporaryValue;
-//   }
-//   pairs = setOfPics
-// }
-
-// function randomImg() {
-//   // console.log("randomImg");
-  // let set = [1, 2, 3, 4, 5, 6, 3, 2, 1, 4, 5, 6];
-//   let pairs = shuffle(set)
-  // if (set.length > 0) {
-  //   for (let i = 0; i < set.length; i++) {
-  //     let randomNumber = Math.floor(Math.random() * setMax);
-  //     console.log(randomNumber)
-  //     result = set[randomNumber]
-  //     setMatch.push(set[randomNumber]);
-  //     set.splice(result, 1);
-  //     return result;
-  //   }
-  // } else if (set.length = 0) {
-  //   for (let i = 0; i < setMatch.length; i++) {
-  //     let randomNumber = Math.floor(Math.random() * setMatchMax);
-  //     result = setMatch[randomNumber];
-  //     setMatch.splice(setMatch[randomNumber], 1);
-  //     return result;
-  //   }
-  // }
+startGame.addEventListener("click", gameStart);
 
 
 function gameStart() {
-  let game = document.querySelector("#game")
-  game.classList.remove("hidden")
+  game.classList.remove("hidden");
   let indices = [
     "1",
     "2",
@@ -124,33 +52,25 @@ function gameStart() {
     "11",
     "12"
   ];
-  // shuffle the order to make it random and store to the pairs array
   let pairs = shuffle(indices);
-  console.log(pairs);
-  // loop over the card divs and assign them randomized image pairs
+
   for (let i = 0; i < imageDivs.length; i++) {
-    // construct image path
     let path = "./images/" + pairs[i] + ".jpg";
-    console.log(path)
-    // target the img child element and set its src to the path
+
     imageDivs[i].children[0].src = path;
-    console.log(imageDivs[i])
+    console.log(imageDivs[i]);
   }
 }
 
-// a function to randomly shuffle the array, from Stack Overflow
 function shuffle(array) {
   var currentIndex = array.length,
     temporaryValue,
     randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
+    while (0 !== currentIndex) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
-    // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
@@ -160,73 +80,78 @@ function shuffle(array) {
 }
 
 
-// **Event listener - find matches**
-
-for (let image of images) {
-  image.addEventListener("click", imageClick);
+for (let card of imageDivs) {
+  card.addEventListener("click", imageClick);
 }
 
 
-  var pairs = {
-    firstCard: null,
-    secondCard: null
-  };
-
-function imageClick(e){
-  e.preventDefault()
-  let currentCard = e.target.parentElement;
-  console.log(currentCard)  
-
-e.target.style.opacity = "100%"
-
-
-  // let compairSrc = e.target.img
-
-  if (pairs.firstCard === null) {
-    pairs.firstCard = e.target;
-    console.log(pairs.firstCard)
-
-  } else if (pairs.firstCard !== null && pairs.firstCard === null) {
-    pairs.secondCard = document.querySelector("img");
-    console.log(pairs.secondCard)
-    if (pairs.firstImage === pairs.secondImage) {
-      match()
-    } else if (pairs.firstImage !== pairs.secondImage) {
-      notAMatch()
+function imageClick(e) {
+  let currentCard = e.currentTarget;
+  if (currentCard.children[0].classList.contains("flipped")) return;
+  if (!pairs.firstCard || !pairs.secondCard) {
+    if (!currentCard.children[0].classList.contains("flipped")) {
+      setScore(currentScore + 1);
+      console.log(currentScore);
     }
-  }
-}
-// **Helper functions - find matches**
-
-
-function match() {
-    alert("Match!");
-    scoreCalc();
-    pairs.firstCard = null;
-    pairs.secondCard = null;
-}
-
-function notAMatch() {
-    alert("Not a match. Try again!");
-    pairs.firstCard = "undefined";
-    pairs.secondCard = "undefined";
-    image.style.opacity = "0";
-}
-
-
-// **Scoring**
-
-// function scoreCalc() {
-//   runningScore += 1;
-//   score.innerHTML = `Score: ${runningScore}`
-
-// }
-
-  function setScore(newScore) {
-    currentScore = newScore;
-    score.innerText = currentScore;
+    currentCard.children[0].classList.add("flipped");
+    pairs.firstCard = pairs.firstCard || currentCard;
+    pairs.secondCard = currentCard === pairs.firstCard ? null : currentCard;
+    console.log(pairs);
   }
 
-  if (lowScore) {
-    document.getElementById("best-score").innerText = lowScore;
+
+  if (pairs.firstCard && pairs.secondCard) {
+    let jpg1 = pairs.firstCard.children[0].src;
+    console.log(jpg1)  
+    let jpg2 = pairs.secondCard.children[0].src;
+    console.log(jpg1);
+
+    if (jpg1 === jpg2) {
+      cardsFlipped += 2;
+      pairs.firstCard.removeEventListener("click", imageClick);
+      pairs.secondCard.removeEventListener("click", imageClick);
+      pairs.firstCard = null;
+      pairs.secondCard = null;
+    } else {
+      setTimeout(function () {
+        console.log(pairs);
+        pairs.firstCard.children[0].classList.remove("flipped");
+        pairs.secondCard.children[0].classList.remove("flipped");
+        pairs.firstCard = null;
+        pairs.secondCard = null;
+      }, 1000);
+    }
+  
+
+  if (cardsFlipped === numCards) {
+    endGame()
   }
+  }
+}
+
+
+function setScore(newScore) {
+  currentScore = newScore;
+  document.getElementById("score").innerText = currentScore;
+}
+
+
+if (lowScore) {
+  document.getElementById("best-score").innerText = lowScore;
+}
+
+function endGame() {
+  let scoreHeader = document.getElementById("header");
+  scoreHeader.innerText = "Game Over";
+  
+  let lowScore = +localStorage.getItem("low-score") || Infinity;
+  if (currentScore < lowScore) {
+    localStorage.setItem("low-score", currentScore)
+  }
+  
+  var restartGame = document.querySelector("button");
+  restartGame.innerText = "Replay"
+  restartGame.addEventListener("click", function() {
+    window.location.reload();
+  })
+}
